66d405bf50612cfe6c2537161c45accb19b5cee2
Payton Quackenbush <payton.quackenbush@gmail.com>
2012-05-07 00:00:06 -0500

    Added 'repeat' operator

--- 02-music/compiler.js
+++ 02-music/compiler.js
@@ -13,6 +13,7 @@ var strip_rest = function(nodes) {
 }
 
 var pitch2midi = function(muspitch) {
+    // Convert musical pitch to MIDI integer
     // 12 + 12 * octave + letterPitch. The letterPitch is 0 for C, 2 for D, up to 11 for B
     var LETTER_PITCHES = {'c': 0,
                           'd': 2,
@@ -21,6 +22,7 @@ var pitch2midi = function(muspitch) {
                           'g': 7,
                           'a': 9,
                           'b': 11};
+
     var letter = muspitch[0];
     var letterPitch = LETTER_PITCHES[letter.toLowerCase()];
     var octave = parseInt(muspitch.slice(1));
@@ -45,6 +47,22 @@ var mus2note = function(start, musexpr) {
         return strip_rest(left.concat(right));
     }
 
+    if (musexpr.tag == 'repeat')
+    {
+        var i;
+        var nodes = []
+        for (i = 0; i < musexpr.count; i++)
+        {
+            var next = mus2note(start, musexpr.section);
+            var last = next[next.length - 1];
+            nodes = nodes.concat(next);
+            start = last.start + last.dur;
+        }
+
+        return nodes;
+    }
+
+    // Either a 'note' or a 'rest'
     var pitch;
 
     if (musexpr.tag == 'note')
@@ -86,11 +104,27 @@ var test = function() {
           { tag: 'note', pitch: pitch2midi('e4'), start: 0, dur: 250 },
           { tag: 'note', pitch: pitch2midi('g4'), start: 100, dur: 250 } ];
 
+    var test1 = { tag: 'repeat',
+                  section: { tag: 'note', pitch: 'c4', dur: 250 },
+                  count: 3 };
+    var exp1 = [ { tag: 'note', pitch: pitch2midi('c4'), start: 0, dur: 250 },
+                 { tag: 'note', pitch: pitch2midi('c4'), start: 250, dur: 250 },
+                 { tag: 'note', pitch: pitch2midi('c4'), start: 500, dur: 250 } ];
+
+
+    // --------------------------------------------------------------------------------
+
     var actual0 = compile(test0);
-    //console.log(actual0);
-    //console.log(exp0);
+    var actual1 = compile(test1);
+
+    console.log("--------------------");
+    if (false) {
+        console.log(actual1);
+        console.log(exp1);
+    }
 
     assert.deepEqual(actual0, exp0);
+    assert.deepEqual(actual1, exp1);
 
     console.log("PASS");
 }
